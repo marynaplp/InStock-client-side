c4138d65f507e695814b2416becfabb56fca800b
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-02-06 14:39:40 -0800

    Add responsive design for header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import './Header.scss';
 export default function Header() {
     return (
         <header className="header">
-            <nav>
+            <nav className="header__logo-container">
                 <img src={logo} alt="instock-logo" className="header__logo"/>
             </nav>
             <nav className="header__nav-bar">
                 <div className="header__active-nav-item">
-                    <span className="header__nav-link">Warehouse</span>
+                    <span className="header__nav-link">Warehouses</span>
                 </div>
                 <span className="header__nav-link header__nav-link--inactive">Inventory</span>
             </nav>
