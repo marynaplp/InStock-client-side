519b85e768085bae4f3acca3293c90a4839e70d2
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-02-05 19:23:10 -0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <Routes>
+    <Route path="/" element="HomePage" />;
+    <Route path="/Warehouse" element="Warehouse-Detail" />;
+    <Route path="/Edit-Warehouse" element="Edit-Warehouse" />;
+    <Route path="/Add-Warehouse" element="Add-Warehouse" />;
+    <Route path="/Delete-Warehouse" element="Delete-Warehouse" />;
+    <Route path="/Inventory-Item" element="Inventory-Item" />;
+    <Route path="/Edit-Inventory" element="Edit-Inventory" />;
+    <Route path="/Add-Inventory" element="Add-Inventory" />;
+    <Route path="/Delete-Inventory" element="Delete-Inventory" />;
+    </Routes>
     </div>
+   </BrowserRouter>
   );
 }
 
