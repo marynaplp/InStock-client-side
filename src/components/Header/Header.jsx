ba8f8aa72e8278da17b7d3486e7984f0ed6bf324
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-02-07 18:49:12 -0800

    detail-header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,22 @@ import './Header.scss';
 
 export default function Header() {
     return (
-        <header className="header">
-            <nav className="header__logo-container">
-                <img src={logo} alt="instock-logo" className="header__logo"/>
-            </nav>
-            <nav className="header__nav-bar">
-                <div className="header__active-nav-item">
-                    <h3 className="header__nav-link">Warehouses</h3>
+<header className="header">
+                <div className="header__container">
+                    <nav className="header__logo-container">
+                        <img src={logo} alt="instock-logo" className="header__logo"/>
+                    </nav>
+                    <nav className="header__nav-bar">
+                        <div className="header__active-nav-item">
+                            <h3 className="header__nav-link">Warehouses</h3>
+                        </div>
+                        <div className="header__inventory">
+                            <h3 className="header__nav-link header__nav-link--inactive">Inventory</h3>
+                        </div>
+                    </nav>
                 </div>
-                <div className="header__inventory">
-                    <h3 className="header__nav-link header__nav-link--inactive">Inventory</h3>
+                <div className="header__extender">
                 </div>
-            </nav>
-        </header>
-    )
+            </header>);
 
 }
--- src/pages/WareHouseDetail/WarehouseDetail.jsx
+++ src/pages/WareHouseDetail/WarehouseDetail.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
+import Navigation from "../../components/Navigation/Navigation"
 import WarehouseUserDetail from '../../components/WarehouseUserDetail/WarehouseUserDetail';
 
 export default function WarehouseDetail() {
     return (
         <>
             <Header/>
+            <Navigation/>
             <WarehouseUserDetail/>
         </>
     )
