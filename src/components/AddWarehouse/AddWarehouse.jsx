66c346eb52cc9672f0b7d2f66f7f4cda45dff4e6
Elyse Desrochers <desrochers.e@gmail.com>
2024-02-08 09:23:18 -0800

    added buttons and styled for tablet and desktop

--- src/components/AddWarehouse/AddWarehouse.jsx
+++ src/components/AddWarehouse/AddWarehouse.jsx
@@ -11,62 +11,67 @@ function AddWarehouse() {
             {/* FORM BEGINS HERE */}
             <form className="add-warehouse__form" action="">
 
-                {/* WAREHOUSE DETAILS */}
-                <div className="form__warehouse">
-                    <h2 className="form__header">Warehouse Details</h2>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">Warehouse Name</h3>
-                        </label>
-                        <input placeholder="Warehouse Name" />
+                <div className="form__container">
+                    {/* WAREHOUSE DETAILS */}
+                    <div className="form__warehouse">
+                        <h2 className="form__header">Warehouse Details</h2>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3>Warehouse Name</h3>
+                            </label>
+                            <input placeholder="Warehouse Name" />
+                        </div>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3 className="form__label">Street Address</h3>
+                            </label>
+                            <input placeholder="Street Address" />
+                        </div>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3 className="form__label">City</h3>
+                            </label>
+                            <input placeholder="City" />
+                        </div>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3 className="form__label">Country</h3>
+                            </label>
+                            <input placeholder="Country" />
+                        </div>
                     </div>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">Street Address</h3>
-                        </label>
-                        <input placeholder="Street Address" />
-                    </div>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">City</h3>
-                        </label>
-                        <input placeholder="City" />
-                    </div>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">Country</h3>
-                        </label>
-                        <input placeholder="Country" />
+                    {/* CONTACT DETAILS */}
+                    <div className="form__contact">
+                        <h2 className="form__header">Contact Details</h2>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3 className="form__label">Contact Name</h3>
+                            </label>
+                            <input placeholder="Contact Name" />
+                        </div>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3 className="form__label">Position</h3>
+                            </label>
+                            <input placeholder="Position" />
+                        </div>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3 className="form__label">Phone Number</h3>
+                            </label>
+                            <input placeholder="Phone Number" />
+                        </div>
+                        <div className="form__input">
+                            <label htmlFor="">
+                                <h3 className="form__label">Email</h3>
+                            </label>
+                            <input placeholder="Email" />
+                        </div>
                     </div>
                 </div>
-
-                {/* CONTACT DETAILS */}
-                <div className="form__contact">
-                    <h2 className="form__header">Contact Details</h2>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">Contact Name</h3>
-                        </label>
-                        <input placeholder="Contact Name" />
-                    </div>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">Position</h3>
-                        </label>
-                        <input placeholder="Position" />
-                    </div>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">Phone Number</h3>
-                        </label>
-                        <input placeholder="Phone Number" />
-                    </div>
-                    <div className="form__input">
-                        <label htmlFor="">
-                            <h3 className="form__label">Email</h3>
-                        </label>
-                        <input placeholder="Email" />
-                    </div>
+                <div className="form__button-container">
+                    <button className="form__cancel">Cancel</button>
+                    <button className="form__add">+ Add Warehouse</button>
                 </div>
             </form>
         </main>
