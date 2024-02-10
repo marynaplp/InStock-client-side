533d8f0573f0b64a551f5a71e6117f006e665a25
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-02-09 23:39:33 -0800

    Fix styling for mobile and tablet

--- src/components/InventoryList/InventoryCard/InventoryCard.jsx
+++ src/components/InventoryList/InventoryCard/InventoryCard.jsx
@@ -8,31 +8,35 @@ export default function InventoryCard({item}) {
     return (
         <section className='inventory-card'>
             <div className="inventory-card__list-container">
-                <div className="inventory-card__container-info">
-                    <h4 className="inventory-card__header">Inventory Item</h4>
-                    <div className="inventory-card__inventory-item">
-                        <p className="inventory-card__item">{item.item_name}</p> 
-                        <img  src={chevron} alt='chevron' />
+                    <div className="inventory-card__container-info">
+                        <h4 className="inventory-card__header">Inventory Item</h4>
+                        <div className="inventory-card__inventory-item">
+                            <p className="inventory-card__item">{item.item_name}</p> 
+                            <img  src={chevron} alt='chevron' />
+                        </div>
+                        <h4 className="inventory-card__header">CATEGORY</h4>
+                        <p className="inventory-card__item">{item.category}</p>
                     </div>
-                    <h4 className="inventory-card__header">CATEGORY</h4>
-                    <p className="inventory-card__item">{item.category}</p>
-                </div>
-                <div className="inventory-card__container-info inventory-card__container-info--inactive ">
-                    <div className='inventory-card__status-container'>
-                    <h4 className="inventory-card__header">Status</h4>
-                    <p className={`inventory-card__item-status ${item.status === 'Out of Stock' ? 'inventory-card__item-status--out-of-stock' : ''}`}>
-                        {item.status.toUpperCase()}
-                    </p>
+                    <div className="inventory-card__container-info inventory-card__container-info--inactive ">
+                        <div className='inventory-card__status-container'>
+                        <h4 className="inventory-card__header">Status</h4>
+                        <p className={`inventory-card__item-status ${item.status === 'Out of Stock' ? 'inventory-card__item-status--out-of-stock' : ''}`}>
+                            {item.status.toUpperCase()}
+                        </p>
+                        </div>
+                        <h4 className="inventory-card__header">QTY</h4>
+                        <p className="inventory-card__item inventory-card__item--active">{item.quantity}</p>
+                        <h4 className="inventory-card__header">WAREHOUSE</h4>
+                        <p className="inventory-card__item inventory-card__item--active" >{item.warehouse_name}</p>
                     </div>
-                    <h4 className="inventory-card__header">QTY</h4>
-                    <p className="inventory-card__item inventory-card__item--active">{item.quantity}</p>
-                    <h4 className="inventory-card__header">WAREHOUSE</h4>
-                    <p className="inventory-card__item inventory-card__item--active" >{item.warehouse_name}</p>
-                </div>
-                <div className="inventory-card__icons">
+                <div className="inventory-card__icons inventory-card__icons--tablet">
                     <img className="inventory-card__edit" src={deleteBtn} alt="delete button" />
                     <img className="inventory-card__edit" src={edit} alt="Edit Warehouse" />
                 </div>
+            </div>
+            <div className="inventory-card__icons inventory-card__icons--mobile">
+                    <img className="inventory-card__edit" src={deleteBtn} alt="delete button" />
+                    <img className="inventory-card__edit" src={edit} alt="Edit Warehouse" />
             </div>
 
         </section>
