de0a3df213bfc4dc36a3c4775b1644d3b30d49a5
madhujamitra <44411291+madhujamitra@users.noreply.github.com>
2024-02-12 11:45:32 -0800

    detail page

--- src/components/WarehouseUserDetail/WarehouseUserDetail.jsx
+++ src/components/WarehouseUserDetail/WarehouseUserDetail.jsx
@@ -1,30 +1,54 @@
 import Navigation from "../Navigation/Navigation";
 import "./WarehouseUserDetail.scss";
+import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function WarehouseUserDetail() {
+  const { warehouseId } = useParams();
+  const { REACT_APP_API_BASE_PATH } = process.env;
+
+  const [warehouseDetail, setwarehouseDetail] = useState([]);
+  useEffect(() => {
+    const url = `${REACT_APP_API_BASE_PATH}/api/warehouses/${warehouseId}`;
+    
+    const getAllWarehouses = async() => {
+      try {
+        const response = await axios.get(url);
+        setwarehouseDetail(response.data);
+      } catch (err) {
+        alert("Error: No warehouse with that id exists.", err);
+      }
+    };
+    getAllWarehouses();
+  }, []);
+
   return (
     <div className="Warehouse">
-      <Navigation/>
+      <Navigation
+      value = {warehouseDetail}
+      />
       <div className="Warehouse__Detail">
         <div className="Warehouse__Detail-address">
           <h4 className="Warehouse__Detail-header">warehouse address:</h4>
           <span>
-            33 Pearl Street SW,
-            <br className="break__line" /> Washington, USA
+          {warehouseDetail.address},
+            <br className="break__line" />{warehouseDetail.city} {warehouseDetail.country}
           </span>
         </div>
         <div className="Warehouse__Detail-contact">
           <div className="Warehouse__Detail-section">
             <h4 className="Warehouse__Detail-header">Contact name:</h4>
-            <span>Graeme Lyon</span>
+            <span>{warehouseDetail.contact_name}</span>
             <br></br>
             <span>Warehouse Manager</span>
           </div>
           <div className="WareHouse__Detail-section">
             <h4 className="Warehouse__Detail-header">Contact information:</h4>
-            <span>+1(647)504-0911</span>
+            <span>{warehouseDetail.contact_phone}</span>
             <br></br>
-            <span>glyon@instock.com</span>
+            <span>{warehouseDetail.contact_email}</span>
           </div>
         </div>
       </div>
